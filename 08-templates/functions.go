f257c8080bc80005fe788385a612f1f5881f23c2
ajsoriar <ajsoriar@gmail.com>
2017-12-11 22:22:23 +0100

    go code and html templates

--- 08-templates/functions.go
+++ 08-templates/functions.go
@@ -1,1 +1,14 @@
-package _8_templates
+package main
+
+import (
+	"strings"
+	"html/template"
+)
+
+var (
+	templateMap = template.FuncMap{
+		"Upper": func(s string) string {
+			return strings.ToUpper(s)
+		},
+	}
+)
