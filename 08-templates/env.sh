e85a06e139be6763d0f8c3ba6c53faab2d4b1fe8
ajsoriar <ajsoriar@gmail.com>
2017-12-11 22:23:56 +0100

    unix script to change environment paths

--- 08-templates/env.sh
+++ 08-templates/env.sh
@@ -1,3 +1,5 @@
+#Use this script this way: > . env.shcd ..
+
 echo " "
 echo "------------------- "
 echo " Script de andres!"
@@ -10,11 +12,11 @@ echo " "
 if [ `type -p go` = "" ]; then
     export PATH=$PATH:/usr/local/go/bin
 fi
-export GOPATH=$PWD
+export GOPATH=$PWD/pkg
 export PATH=$PATH:$PWD/bin
 
 go env
 
 echo " "
-echo "The end ..."
+echo "The end... GOPATH was set!"
 echo " "
