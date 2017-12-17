83e47eb11e8f8f1e0d0166183d2bf41ffcf66882
ajsoriar <ajsoriar@gmail.com>
2017-12-17 19:49:48 +0100

    create black image

--- 10-base64-to-image/create-image.2.go
+++ 10-base64-to-image/create-image.2.go
@@ -3,12 +3,35 @@ package main
 import (
     "image"
     "image/png"
+    
+
+
+    "image/color"
+
+    //"math"
+
+    
     "os"
 )
 
 func main() {
-    // Create a blank image 100x200 pixels
-    myImage := image.NewRGBA(image.Rect(0, 0, 100, 200))
+
+    var w, h int = 280, 240
+
+    // Create a blan image 100x200 pixels
+    myImage := image.NewRGBA(image.Rect(0, 0, w, h))
+    for x := 0; x < w; x++ {
+        for y := 0; y < h; y++ {
+            c := color.RGBA{
+                0,
+                0,
+                0,
+                255,
+            }
+            myImage.Set(x, y, c)
+        }
+    }
+
 
     // outputFile is a File type which satisfies Writer interface
     outputFile, err := os.Create("test.png")
